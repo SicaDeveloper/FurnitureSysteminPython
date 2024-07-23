89990240ee9b5da3147d1ac0d4643cb356df35c4
SicaDeveloper <rameshoer827@gmail.com>
2024-07-23 10:09:44 +0545

    added while loop to adding more items and emptied write file

--- operations.py
+++ operations.py
@@ -1,32 +1,35 @@
 import read
 
+status = True
 def addFurniture():
-    furnitureValue = input("Is this piece of furniture already in the inventory? (yes/no) \n")
-    
-    if furnitureValue.lower() == "yes":
-        checkId = input("Input the ID of the furniture \n")
-        for line in range(len(read.inventory)):
-            item = read.inventory[line]
-            if checkId in item:
-                newCheckId = int(checkId) - 1
-                newQuantity = int(input("Enter the amount of new inventory \n"))
-                addedValue = int(read.inventory[newCheckId][checkId][2]) + newQuantity
-                read.inventory[newCheckId][checkId][2] = addedValue
-                print("Inventory added successfully \n")
-                
-    elif furnitureValue.lower() == "no":
-        furnitureManufacturer = input("Enter Manufacturer Name: ")
-        furnitureType = input("Enter Furniture Type: ")
-        furnitureQuantity = int(input("Enter Quantity: "))
-        furniturePrice = input("Enter Price per Unit: ")
+    while status == True:
+        furnitureValue = input("Is this piece of furniture already in the inventory? (yes/no) \n")
         
-        lastId = int(read.recentId) + 1
-        
-        
-        read.inventory.append(
-            {
-                str(lastId) : [furnitureManufacturer,furnitureType,furnitureQuantity,furniturePrice]
-            }
-        )
-    else:
-        print("Error try again")
+        if furnitureValue.lower() == "yes":
+            checkId = input("Input the ID of the furniture \n")
+            for line in range(len(read.inventory)):
+                item = read.inventory[line]
+                if checkId in item:
+                    newCheckId = int(checkId) - 1
+                    newQuantity = int(input("Enter the amount of new inventory \n"))
+                    addedValue = int(read.inventory[newCheckId][checkId][2]) + newQuantity
+                    read.inventory[newCheckId][checkId][2] = addedValue
+                    
+        elif furnitureValue.lower() == "no":
+            furnitureManufacturer = input("Enter Manufacturer Name: ")
+            furnitureType = input("Enter Furniture Type: ")
+            furnitureQuantity = int(input("Enter Quantity: "))
+            furniturePrice = input("Enter Price per Unit: ")
+            
+            lastId = int(read.recentId) + 1
+            
+            read.inventory.append(
+                {
+                    str(lastId): [furnitureManufacturer, furnitureType, furnitureQuantity, furniturePrice]
+                }
+            )
+        else:
+            print("Error try again")
+    statusCheck = input("Do you want to add more items \n")
+    if statusCheck.lower() == "no":
+        status == False
