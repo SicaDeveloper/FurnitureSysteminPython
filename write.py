d1aa72272b60bc7a44c04d592362374d0c443094
SicaDeveloper <rameshoer827@gmail.com>
2024-08-01 20:10:28 +0545

    file creation and its FINALLY FUNCTIONAL

--- write.py
+++ write.py
@@ -2,9 +2,10 @@ import datetime
 
 time = datetime.datetime.now()
 receipt = None
-timeString = str(time).replace() + " Receipt"
+timeString = str(time).split()[0] + " Receipt"
 def writeReceipt():
     fileName = timeString + ".txt"
     receiptFile = open(fileName, "w")
-    receiptFile.write(receipt)
+    for item in receipt:
+        receiptFile.write(str(item))
     receiptFile.close()
