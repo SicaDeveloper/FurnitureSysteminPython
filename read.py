380b65493c5697d13ddd268496220cd37403240a
SicaDeveloper <rameshoer827@gmail.com>
2024-08-12 19:01:00 +0545

    split changes

--- read.py
+++ read.py
@@ -5,7 +5,7 @@ try:
 except:
     print("File not found,please create the required file [text.txt]")
 for line in inventoryFile:
-    segment = line.strip().split(',')
+    segment = line.split(',')
     inventory.update({segment[0]:[segment[1],segment[2],segment[3],segment[4]]})
     operations.recentId = int(segment[0])
 def readInventory():
